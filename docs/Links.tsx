20420d80aaf2e4497cf5c358e5952980de037e3b
Rico Kahler <ricokahler@me.com>
2020-01-18 14:07:00 -0500

    add color example

--- docs/Links.tsx
+++ docs/Links.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, PropsFromStyles, Anchor } from 'hacker-ui';
+import { createStyles, PropsFromStyles, Anchor, useTheme } from 'hacker-ui';
 import { Link } from 'react-router-dom';
 
 const useStyles = createStyles(({ css, theme }) => ({
@@ -18,13 +18,23 @@ interface Props extends PropsFromStyles<typeof useStyles> {}
 
 function Links(props: Props) {
   const { Root, styles } = useStyles(props);
+  const theme = useTheme();
+
   return (
     <Root>
       <h1 className={styles.title}>Links</h1>
       <p className={styles.text}>
         <Anchor href="#">This</Anchor> is a link. Links are stuff and things.{' '}
         <Anchor component={(props: any) => <Link to="/links" {...props} />}>
-          React router link
+          This is a <code>react-router</code> link.
+        </Anchor>{' '}
+        This link is a different{' '}
+        <Anchor color={theme.colors.accent} href="#">
+          color
+        </Anchor>
+        .{' '}
+        <Anchor href="#" color={theme.colors.danger}>
+          Danger link
         </Anchor>
       </p>
     </Root>
