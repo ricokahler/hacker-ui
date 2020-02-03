e51804bf0e56796d5d7853761405251ba18a7ace
Rico Kahler <ricokahler@me.com>
2020-02-02 19:22:24 -0500

    add links example

---
+++ examples/Links.example.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { createStyles, PropsFromStyles, Anchor, useTheme } from 'hacker-ui';
+
+const useStyles = createStyles(({ css, theme }) => ({
+  root: css`
+    & > *:not(:last-child) {
+      margin-bottom: ${theme.space(1)};
+    }
+  `,
+}));
+
+interface Props extends PropsFromStyles<typeof useStyles> {}
+
+function LinksExample(props: Props) {
+  const { Root } = useStyles(props);
+  const theme = useTheme();
+
+  return (
+    <Root>
+      <div>
+        This is a simple <Anchor href="#">link</Anchor>.
+      </div>
+
+      <div>
+        You can also change the color of the link{' '}
+        <Anchor href="#" color={theme.colors.brand}>
+          link
+        </Anchor>{' '}
+        using the standard <code>color</code> prop.
+      </div>
+
+      <div>
+        <Anchor href="#" color={theme.colors.danger}>
+          Danger link
+        </Anchor>{' '}
+      </div>
+
+      <div>
+        <Anchor href="#" color={theme.colors.warning}>
+          Warning link
+        </Anchor>
+      </div>
+    </Root>
+  );
+}
+
+export default LinksExample;
