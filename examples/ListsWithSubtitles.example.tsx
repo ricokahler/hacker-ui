654da496d4885df1ac0fb358fe56fc1db74a9a56
Rico Kahler <ricokahler@me.com>
2020-02-09 17:02:27 -0500

    add list with subtitles example

---
+++ examples/ListsWithSubtitles.example.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  createStyles,
+  PropsFromStyles,
+  List,
+  ListItem,
+  ListItemButton,
+} from 'hacker-ui';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const useStyles = createStyles(({ css, theme }) => ({
+  root: css``,
+  list: css`
+    width: ${theme.block(3)};
+    background-color: ${theme.colors.surface};
+    margin: ${theme.gap(1)} auto;
+    box-shadow: ${theme.shadows.standard};
+  `,
+  listItemButton: css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    overflow: hidden;
+    text-align: left;
+  `,
+  info: css`
+    flex: 1 1 auto;
+  `,
+  title: css`
+    ${theme.fonts.h5}
+  `,
+  subtitle: css`
+    ${theme.fonts.caption}
+  `,
+  icon: css`
+    flex: 0 0 auto;
+  `,
+}));
+
+interface Props extends PropsFromStyles<typeof useStyles> {}
+
+function ListWithSubtitlesExample(props: Props) {
+  const { Root, styles } = useStyles(props);
+
+  return (
+    <Root>
+      <List className={styles.list}>
+        <ListItem>
+          <ListItemButton className={styles.listItemButton}>
+            <div className={styles.info}>
+              <div className={styles.title}>Apples</div>
+              <div className={styles.subtitle}>
+                Updated at {new Date().toLocaleTimeString()}
+              </div>
+            </div>
+            <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton className={styles.listItemButton}>
+            <div className={styles.info}>
+              <div className={styles.title}>Bananas</div>
+              <div className={styles.subtitle}>
+                Updated at {new Date().toLocaleTimeString()}
+              </div>
+            </div>
+            <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton className={styles.listItemButton}>
+            <div className={styles.info}>
+              <div className={styles.title}>Oranges</div>
+              <div className={styles.subtitle}>
+                Updated at {new Date().toLocaleTimeString()}
+              </div>
+            </div>
+            <FontAwesomeIcon className={styles.icon} icon={faChevronRight} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Root>
+  );
+}
+
+export default ListWithSubtitlesExample;
