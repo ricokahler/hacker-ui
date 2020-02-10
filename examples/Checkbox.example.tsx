c157f7e08014e55eacc175daa7a723c4562715aa
Rico Kahler <ricokahler@me.com>
2020-02-09 23:46:41 -0500

    add checkbox example

---
+++ examples/Checkbox.example.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  createStyles,
+  PropsFromStyles,
+  FormControl,
+  Label,
+  HelperText,
+  Checkbox,
+} from 'hacker-ui';
+
+const useStyles = createStyles(({ css, theme }) => ({
+  root: css`
+    display: flex;
+    margin: ${theme.gap(1)} auto;
+    flex-wrap: wrap;
+    & > *:not(:last-child) {
+      margin-right: ${theme.gap(1)};
+      margin-bottom: ${theme.space(1)};
+    }
+  `,
+  row: css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    & > *:not(:last-child) {
+      margin-right: ${theme.space(1)};
+    }
+  `,
+}));
+
+interface Props extends PropsFromStyles<typeof useStyles> {}
+
+function CheckboxExample(props: Props) {
+  const { Root, styles } = useStyles(props);
+
+  return (
+    <Root>
+      <FormControl>
+        <div className={styles.row}>
+          <Checkbox />
+          <Label>Enable</Label>
+        </div>
+        <HelperText>Checkbox then label</HelperText>
+      </FormControl>
+
+      <FormControl>
+        <div className={styles.row}>
+          <Label>Agree</Label>
+          <Checkbox />
+        </div>
+        <HelperText>Label then checkbox</HelperText>
+      </FormControl>
+
+      <FormControl className={styles.row}>
+        <Checkbox />
+        <Label>No helper text</Label>
+      </FormControl>
+    </Root>
+  );
+}
+
+export default CheckboxExample;
