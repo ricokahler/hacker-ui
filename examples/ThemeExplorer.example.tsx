46f33dba9c679b737e5b787ed42e7f5eca905fa5
Rico Kahler <ricokahler@me.com>
2020-02-14 02:13:36 -0500

    add default theme explorer example

---
+++ examples/ThemeExplorer.example.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ObjectInspector } from 'react-inspector';
+import { useTheme, createStyles, PropsFromStyles } from 'hacker-ui';
+
+const useStyles = createStyles(({ css, theme }) => ({
+  root: css`
+    background-color: ${theme.colors.surface};
+    margin: ${theme.gap(1)};
+    box-shadow: ${theme.shadows.standard};
+    padding: ${theme.space(1)};
+  `,
+}));
+
+interface Props extends PropsFromStyles<typeof useStyles> {}
+
+function ThemeExplorerExample(props: Props) {
+  const { Root } = useStyles(props);
+  const theme = useTheme();
+
+  return (
+    <Root>
+      <ObjectInspector data={theme} />
+    </Root>
+  );
+}
+
+export default ThemeExplorerExample;
