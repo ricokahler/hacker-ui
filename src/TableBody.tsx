c248f22257a59c4675c3d171feaaad2a58030fae
Pearl Zeng <zhuzeng@umich.edu>
2020-03-31 08:36:07 -0400

    Update src/TableBody.tsx
    Co-Authored-By: Rico Kahler <ricokahler@me.com>

--- src/TableBody.tsx
+++ src/TableBody.tsx
@@ -1,21 +1,3 @@
-import React, { forwardRef } from 'react';
-import createStyles from './createStyles';
-import { PropsFromStyles } from './types';
-
-const useStyles = createStyles(({ css, theme }) => ({
-  // table body base styles
-  root: css``,
-}));
-
-type TableBodyProps = JSX.IntrinsicElements['tbody'];
-
-interface Props extends PropsFromStyles<typeof useStyles>, TableBodyProps {}
-
-const TableBody = forwardRef((props: Props, ref: React.Ref<any>) => {
-  const { Root, styles, ...restOfProps } = useStyles(props, 'tbody');
-  return <Root ref={ref} {...restOfProps} />;
-});
-
-TableBody.displayName = 'TableBody';
+const TableBody = 'tbody';
 
 export default TableBody;
